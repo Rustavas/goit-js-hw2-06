3c1625f1321ce6b454fcf3994187835239a3536d
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-06-15 15:10:37 +0300

    Added start files

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categories = document.querySelector('#categories'); 
+console.log(categories.children)
+const numberOfCategories = 0;
+categories.children.forEach(value => console.log(value));
+console.log(numberOfCategories)
+// categories.children.forEach((value, index, array) => {})
+// console.log('Number of categories:', categories.children.length);
